6b6338fb4ea94467526e575a536931683d3ab41a
YangEunji <129482143+heubanufi@users.noreply.github.com>
2023-08-12 19:15:23 +0900

    Header, Textbook - Link to

--- src/pages/Education/Textbook.jsx
+++ src/pages/Education/Textbook.jsx
@@ -3,35 +3,38 @@ import "./Textbook.scss";
 import Header from 'systems/Header';
 import Footer from 'components/Footer';
 import {ReactComponent as Search} from "../../assets/images/search.svg";
+import { Link } from 'react-router-dom';
 
 export default function Textbook() {
     const [textbook] = useState ([
         {
-            tbDate: "2023.8.11금",
-            tbTitle: "기업 실적 분석에 쓰이는 재무재표에 대해 알아볼까요?",
+            tbDate: "2023.8.11.금",
+            tbTitle: "기업 실적 분석에 쓰이는 재무제표에 대해 알아볼까요?",
             tbThumbnail: "",
             tbUrl: "",
         },
         {
-            tbDate: "2023.8.11금",
+            tbDate: "2023.8.11.금",
             tbTitle: "주요 지수, 코스피 코스닥이란?",
             tbThumbnail: "",
         },
         {
-            tbDate: "2023.8.11금",
+            tbDate: "2023.8.11.금",
             tbTitle: "주식 기본 용어, 알고 시작하자!",
             tbThumbnail: "",
         },
         
     ]);
 
-    const textbookItem = textbook.map((item) => (
-        <div className="tbList">
-            <div className="tbItems" onClick={() => window.open("")}>                
-                <div className="tbItemTitle">{item.tbTitle}</div>
-                <div className="tbItemDate">{item.tbDate}</div>
-            </div>
-            <img alt="썸네일" className="tbImage"/>
+    const textbookItem = textbook.map((item, index) => (
+        <div className="tbList" key={index}>            
+                <div className="tbItems">  
+                <Link to={`/tbDetail${index + 1}`} style={{ textDecoration: 'none' }}>          
+                    <div className="tbItemTitle">{item.tbTitle}</div>
+                    </Link>
+                    <div className="tbItemDate">{item.tbDate}</div>
+                </div>            
+                <img alt="썸네일" className="tbImage"/>      
         </div>
     ))
 
